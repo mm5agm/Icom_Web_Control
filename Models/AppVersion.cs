116d1b7449d08aa401ef3f2b841275acdb4f898b
mm5agm <mm5agm@outlook.com>
2026-06-21 23:18:18 +0100

    Release v2.3.9-pre8: bump version files

--- Models/AppVersion.cs
+++ Models/AppVersion.cs
@@ -2,9 +2,9 @@ namespace Yaesu_Web_Control;
 
 public static class AppVersion
 {
-    public const string Current = "2.3.9-pre7";
+    public const string Current = "2.3.9-pre8";
 
     /// <summary>Date this version was released, ISO format.
     /// Bump on actual release; current value reflects the planned ship date.</summary>
-    public const string ReleaseDate = "2026-06-18";
+    public const string ReleaseDate = "2026-06-22";
 }
